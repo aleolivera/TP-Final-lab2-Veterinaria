16765182317db55f75f03e0603b2468bd782f2b0
aleolivera <aleoliverarp10@gmail.com>
2020-06-05 14:47:42 -0300

    libreria ctime
    objeto fecha terminado

--- TP Final Lab2 Veterinaria/fecha.cpp
+++ TP Final Lab2 Veterinaria/fecha.cpp
@@ -19,6 +19,17 @@ void Fecha::getFechaActual(){
     dia = p->tm_mday;
     mes = p->tm_mon+1;
     anio= p->tm_year +1900;
+
+//    struct tm
+//    tm_sec	int	seconds after the minute	0-61*
+//    tm_min	int	minutes after the hour	0-59
+//    tm_hour	int	hours since midnight	0-23
+//    tm_mday	int	day of the month	1-31
+//    tm_mon	int	months since January	0-11
+//    tm_year	int	years since 1900
+//    tm_wday	int	days since Sunday	0-6
+//    tm_yday	int	days since January 1	0-365
+//    tm_isdst int	Daylight Saving Time flag
 }
 void Fecha::cargarFecha(){
     cout << "DIA/MES/ANIO: ";
